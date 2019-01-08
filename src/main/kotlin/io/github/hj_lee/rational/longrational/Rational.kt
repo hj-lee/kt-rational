e335110af8938a2f1e4f38158391a2484024c877
Hwijae Lee <hj.d.lee@gmail.com>
2019-01-08 14:26:24 -0500

    Fix BigInteger typo in longrational

--- src/main/kotlin/io/github/hj_lee/rational/longrational/Rational.kt
+++ src/main/kotlin/io/github/hj_lee/rational/longrational/Rational.kt
@@ -1,7 +1,6 @@
 package io.github.hj_lee.rational.longrational
 
 import java.io.Serializable
-import java.math.BigInteger
 import kotlin.math.sign
 
 fun Long.toRational() = Rational(this, 1L)
@@ -17,15 +16,15 @@ fun Double.toRational(): Rational {
     val bits = this.toBits()
     var exponent = (bits.and(BUT_SIGN_BIT).ushr(52) - 1023 - 52).toInt()
     var fraction = bits.and(FRACTION_MASK).or(FRACTION_1)
-    require (exponent < 11) { "Double number is too big"}
-    if (exponent < -(62+52)) return Rational.ZERO
+    require(exponent < 11) { "Double number is too big" }
+    if (exponent < -(62 + 52)) return Rational.ZERO
 //    println(exponent)
 //    println(fraction.toString(2))
     if (exponent < -62) {
-        fraction = fraction.shr(-(exponent+62))
+        fraction = fraction.shr(-(exponent + 62))
         exponent = -62
     }
-    val abs = if (exponent >=0 ) Rational(fraction * 1L.shl(exponent), 1L)
+    val abs = if (exponent >= 0) Rational(fraction * 1L.shl(exponent), 1L)
     else Rational(fraction, 1L.shl(-exponent))
     return if (bits < 0) -abs else abs
 }
@@ -66,8 +65,8 @@ fun gcd(a: Long, b: Long): Long = if (b == 0L) a else gcd(b, a % b)
 class Rational private constructor(val numerator: Long, val denominator: Long) : Number(),
     Comparable<Rational>, Serializable {
     companion object {
-        val ZERO = Rational(BigInteger.ZERO, BigInteger.ONE)
-        val ONE = Rational(BigInteger.ONE, BigInteger.ONE)
+        val ZERO = Rational(0L, 1L)
+        val ONE = Rational(1L, 1L)
         operator fun invoke(numer: Long, denom: Long): Rational {
             require(denom != 0L) { "Denominator cannot be 0" }
             val gcd = gcd(numer, denom).let { if (denom.sign * it.sign < 0) -it else it }
